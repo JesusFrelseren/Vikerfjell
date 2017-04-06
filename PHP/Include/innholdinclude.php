37259632c3b19b9aac6104a54f1457a6cdb42d2d
Alexander <alex1990pettersen@gmail.com>
2017-04-06 13:39:22 +0200

    innhold

---
+++ PHP/Include/innholdinclude.php
@@ -0,0 +1,28 @@
+<?php
+$stmt = $mysqli->prepare("SELECT * FROM vikerfjell.innhold ORDER BY idinnhold DESC LIMIT 1;");
+mysqli_set_charset($mysqli, "UTF8");
+$stmt->execute();
+$result = $stmt->get_result();
+$row = $result->fetch_assoc();
+
+$overskrift = $row['tittel'];
+$text = $row['text'];
+$id = $row['idinnhold'];
+
+
+
+$stmt = $mysqli->prepare("SELECT * FROM vikerfjell.bilderinnhold join bilder on _idbilder = idbilder where _idinnhold = $id;");
+mysqli_set_charset($mysqli, "UTF8");
+$stmt->execute();
+$result = $stmt->get_result();
+$row = $result->fetch_assoc();
+$bilde = $row['hvor'];
+
+echo("
+	<div class='staticinnhold'>
+	<H1>$overskrift</h1>
+	<img src=$bilde width=100% height=auto>
+	<p>$text</p>
+	</div>
+	");
+?>
