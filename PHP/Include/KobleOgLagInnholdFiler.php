710e81579d8d6ea797c91169339c377f4645e14d
Alexander <alex1990pettersen@gmail.com>
2017-04-07 13:39:09 +0200

    fix

---
+++ PHP/Include/KobleOgLagInnholdFiler.php
@@ -0,0 +1,25 @@
+﻿<?php
+
+
+
+		$stmt3 = $mysqli->prepare("SELECT * FROM vikerfjell.innhold where idmeny = ?");
+		mysqli_set_charset($mysqli, "UTF8");
+		$stmt3->bind_param("i",$_POST['menylistephp']);
+		$stmt3->execute();
+		$result3 = $stmt3->get_result();
+		$count = 0;
+		while($row3 = $result3->fetch_assoc()){
+		$count ++;
+		}
+		if ($count > 1){
+			include("NyArtikkel.php");
+
+		}else{
+		include("createInnholdFile.php");
+		koblemeny($_POST['menylistephp']);
+		
+		}
+
+
+
+?>
