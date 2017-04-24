bb28858ce3831be94c5220fc99458c2fa2e3709c
Erik <Erik>
2017-04-24 12:57:00 +0200

    DetteBørJegIkkeGjøre

--- PHP/Include/createInnholdFile.php
+++ PHP/Include/createInnholdFile.php
@@ -12,28 +12,29 @@ function lagSide() {
 			return $includes;
 		}
 
-		function koblemeny($id){
-			global $mysqli;
-			mysqli_set_charset($mysqli, "UTF8");		
-
+	function koblemeny($id, $teller){
+		global $mysqli;
+		mysqli_set_charset($mysqli, "UTF8");		
 		//Test ny substring sub id
-		if(strpos($id, 'SUB') !== false) {
-			$nymenyid = substr($id,3);
+		if($teller == 1) {
 			$stmt4 = $mysqli->prepare("SELECT * FROM vikerfjell.innhold RIGHT JOIN vikerfjell.submeny ON innhold.idmeny = submeny.meny_idmeny WHERE submeny.idsubmeny = ?;");
-			$stmt4->bind_param("i",$nymenyid);
+			$stmt4->bind_param("i",$id);
 			$stmt4->execute();
 			$result4 = $stmt4->get_result();
 			$row4 = $result4->fetch_assoc();
 			
 			$overskrift4 = $row4['tittel'].'.html';
+			
+			$overkrift5 = substr($overskrift4, 2);
+			
+			$sql = "UPDATE vikerfjell.submeny set sub_side = ? WHERE idsubmeny = $id";
 
-			$sql = "UPDATE vikerfjell.submeny set sub_side = ? WHERE idsubmeny = $nymenyid";
 			mysqli_set_charset($mysqli, "UTF8");	
 			$stmt = $mysqli->prepare($sql);
-			$stmt->bind_param('s', $overskrift4);  
+			$stmt->bind_param('s', $overskrift5);  
             $stmt->execute();
 
-			$sideInsert = "../../".$overskrift4;
+			$sideInsert = "../../".$overskrift5;
 			$fh = fopen($sideInsert, 'w', 'w');
 			$includes = lagSide();
 			fwrite($fh, $includes);
