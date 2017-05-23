f0a7d95d37dd0663b84af1d19ab5d0f63c96551b
Erlend Hall <erlendahall@gmail.com>
2017-05-23 12:10:59 +0200

    todo

---
+++ todo.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * For bildeside:
+ * todo: endre bildetittel
+ * todo: legg til tooltips
+ * todo: endre bilde
+ * todo: bug: første thumbnail vises ikke
+ * todo: linkmodus-knapp: Fiks getter
+ *
+ *
+ * generelt:
+ * todo: scrolling
+ * todo: Linux-kompatibel
+ * todo: Nettleser kompatibel
+ * todo: Må kjøre fra hvilken som helst lokasjon
+ * todo: breaking bilder på forside
+ *
+ *
+ * ikke så viktig:
+ * todo: fiks hamburgermeny scrolling
+ * todo: breaking på administrer innhold
+ * todo: mer innhold på hjem
+ * todo: knapper highlight i backend
+ */
+
